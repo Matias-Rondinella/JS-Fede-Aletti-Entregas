c28e48082c1f6a7e8956bf96c9c5636de521b564
Matias <matiasrondinellaphoto@gmail.com>
2022-02-08 17:15:38 -0300

    avanzando sobre el codigo

--- assets/js/salud.js
+++ assets/js/salud.js
@@ -1,5 +1,6 @@
-
 let carritoDeCompras = []
+
+//SELECTORES DE ID // DOM
 
 const contenedorTurnos = document.getElementById('contenedor-turnos');
 const turnosCarrito = document.getElementById('carrito-turnos');
@@ -12,8 +13,8 @@ const buscador = document.getElementById('search')
 
 
 
-//filtro
-selecTurnos.addEventListener('change',()=>{
+//filtrado por Select
+/*selecTurnos.addEventListener('change',()=>{
     console.log(selecTurnos.value)
     if(selecTurnos.value == 'Todos'){
         mostrarTurnos(turnoStock)
@@ -21,7 +22,7 @@ selecTurnos.addEventListener('change',()=>{
         mostrarTurnos(turnoStock.filter(el => el.dia == selecTurnos.value))
         console.log(turnoStock.filter(el => el.dia == selecTurnos.value))
     }
-})
+})*/
 
 //Buscador
 buscador.addEventListener('input', ()=>{
@@ -39,31 +40,31 @@ mostrarTurnos(turnoStock)
 
 function mostrarTurnos(array){
    contenedorTurnos.innerHTML ='';
-    for (const turno of array) {
+    for (const listaTurnos of array) {
         let div = document.createElement('div');
-        div.className = 'turno';
+        div.className = 'listaTurnos';
         div.innerHTML += `<div class="card">
                             <div class="card-image">
-                                <img src=${turno.img}>
-                                <span class="card-title">${turno.nombre}</span>
-                                <a id="botonAgregar${turno.id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                                <img src=${listaTurnos.img}>
+                                <span class="card-title">${listaTurnos.area}</span>
+                                <a id="botonAgregar${listaTurnos.id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
                             </div>
                             <div class="card-content">
-                                <p>${turno.desc}</p>
-                                <p>Talle: ${turno.talle}</p>
-                                <p> $${turno.precio}</p>
+                                <p>${listaTurnos.dia}</p>
+                                <p>${listaTurnos.horario}</p>
+                                <p>${listaTurnos.precio}</p>
                             </div>
                         </div> `
                         
         contenedorTurnos.appendChild(div);
         
         
-        let btnAgregar = document.getElementById(`botonAgregar${turno.id}`)
+        let btnAgregar = document.getElementById(`botonAgregar${listaTurnos.id}`)
         // console.log(btnAgregar)
     
         btnAgregar.addEventListener('click',()=>{
 
-            agregarAlCarrito(turno.id)
+            agregarAlCarrito(listaTurnos.id)
         })
     }
     
@@ -123,10 +124,6 @@ function agregarAlCarrito(id) {
 //         + "<h3> Contraseña: </h3>" 
 //         + `${pass}`
 //     }
-
-//SELECTORES DE ID // DOM
-const contenedorTurnos = document.getElementById("contenedorTurnos");
-const selectTurnos = document.getElementById("selectTurnos");
 
 selectTurnos.addEventListener("change",()=>{
     console.log(selectTurnos.value)
